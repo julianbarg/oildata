4b39aff375edde5fdf011d0ec16b3ad0176413c6
julianbarg <barg.julian@gmail.com>
2019-12-07 11:58:40 -0500

    Changed variable name in m_as dataset to be more explicit.

--- R/m_as.R
+++ R/m_as.R
@@ -8,7 +8,7 @@
 #'   \url{https://github.com/julianbarg/oil_industry/blob/master/workbooks/8py_resolve\%20issues.ipynb}
 #' @format 156 observations of companies that are part of a group or have been part of an M&A:
 #' \describe{
-#'   \item{name}{Name of the company group.}
+#'   \item{group_name}{Name of the company group.}
 #'   \item{members}{Name of the member(s).}
 #'   \item{start_year}{Year of the M&A (actually, year of 1st of January that the date of the M&A is closed to.)}
 #'   \item{end_year}{Year that the company was spun off.}
