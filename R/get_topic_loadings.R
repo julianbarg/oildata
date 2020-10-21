646664455d72b24ccfeb3760008482c2303473b5
Julian Barg <barg.julian@gmail.com>
2020-10-21 10:49:35 -0400

    Fixed issue with function for aggregating topic loadings (hopefully).

--- R/get_topic_loadings.R
+++ R/get_topic_loadings.R
@@ -13,14 +13,20 @@
 get_topic_loadings <- function(df = oildata::incidents, variable = volume){
   id_vars <- c("incident_ID", "ID", "year", "commodity", "on_offshore")
   df %>%
-    select({{ id_vars}}, {{ variable }}, starts_with("topic_")) %>%
+    select({{ id_vars }}, {{ variable }}, starts_with("topic_")) %>%
     filter(!is.na({{ variable }}), {{ variable }} != 0, !is.na(topic_1)) %>%
     pivot_longer(starts_with("topic_"), names_to = "topic", values_to = "gamma",
                  names_prefix = "topic_") %>%
+    # Include topic below, too, since the loss is duplicated for every topic
+    # row. This saves us a pivot_wider or dividing by 23 to remove duplicates.
+    # This way, the mathematical operations only include operations within the
+    # same topic and there are no duplicate observations within the mathematical
+    # operation.
     group_by(ID, year, commodity, on_offshore, topic) %>%
     mutate(variable_total = sum({{ variable }}),
            variable_share = {{ variable }} / variable_total,
            topic_share = gamma * variable_share) %>%
-    select(-c(variable_total, variable_share)) %>%
+    group_by(ID, year, commodity, on_offshore, topic) %>%
+    summarize(topic_share = sum(topic_share)) %>%
     ungroup()
 }
