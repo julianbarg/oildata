f6fd4fafa14a376db65af375c348e2c6e750cf08
Julian Barg <barg.julian@gmail.com>
2020-10-21 10:53:34 -0400

    Fixed note on variable binding.

--- R/globals.R
+++ R/globals.R
@@ -1,4 +1,4 @@
 utils::globalVariables(
   c("ID", "name", "year", "commodity", "type", "members", "start_year",
     "end_year", "group_name", "volume", "topic_1", "on_offshore", "topic",
-    "variable_total", "variable_share"))
+    "variable_total", "variable_share", "topic_share"))
